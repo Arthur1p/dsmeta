28cc6d6883f6a364b01354e3501a2c8a711442fe
Arthur Formiga <arthurformigademelo2@gmail.com>
2022-09-05 15:50:39 -0300

    other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
-import NotificatioButton from "./components/notificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+
 
 function App() {
   return (
     <>
-    <h1>Olá mundo!</h1>
-    <NotificatioButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+
+          </div>
+        </section>
+      </main>
     </>
   )
 }
